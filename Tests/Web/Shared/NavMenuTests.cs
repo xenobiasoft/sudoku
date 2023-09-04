85612c4284b34ecfd4c3348fa1c9cda641e16de5
Shannon Stewart <shannon.stewart@outlook.com>
2023-09-04 09:25:53 -0600

    Fixed broken unit test

--- Tests/Web/Shared/NavMenuTests.cs
+++ Tests/Web/Shared/NavMenuTests.cs
@@ -6,7 +6,7 @@ public class NavMenuTests : TestContext
 	public void NavMenu_RendersCorrectly()
 	{
 		// Arrange
-		var expected = "<div class=\"top-row ps-3 navbar navbar-dark\"><div class=\"container-fluid\"><a class=\"navbar-brand\" href>XenobiaSoft Sudoku</a><button title=\"Navigation menu\" class=\"navbar-toggler\" blazor:onclick=\"1\" b-jlhp5ntr5c><span class=\"navbar-toggler-icon\" b-jlhp5ntr5c></span></button></div></div>\r\n\r\n<div class=\"collapse\" blazor:onclick=\"2\" b-jlhp5ntr5c><nav class=\"flex-column\" b-jlhp5ntr5c><div class=\"nav-item px-3\" b-jlhp5ntr5c><a href=\"\" class=\"nav-link active\"><span class=\"oi oi-home\" aria-hidden=\"true\"></span>Home</a></div></nav></div>";
+		var expected = "<div class=\"top-row ps-3 navbar navbar-dark\" ><div class=\"container-fluid\" ><a class=\"navbar-brand\" href=\"\" ><img alt=\"XenobiaSoft Sudoku\" src=\"images/logo.png\" ></a><button title=\"Navigation menu\" class=\"navbar-toggler\" ><span class=\"navbar-toggler-icon\" ></span></button></div>\r\n</div>\r\n<div class=\"collapse\" ><nav class=\"flex-column\" ><div class=\"nav-item px-3\" ><a href=\"\" class=\"nav-link active\"><i class=\"fa-solid fa-house\" ></i>Home</a></div></nav></div>";
 
 		// Act
 		var content = RenderComponent<NavMenu>();
