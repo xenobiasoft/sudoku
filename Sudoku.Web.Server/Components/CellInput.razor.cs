ee19e309902a8530fe6a38645a64d3ca54333f6b
Shannon Stewart <shannon.stewart@outlook.com>
2023-09-13 16:00:54 -0600

    Created CellInput component

---
+++ Sudoku.Web.Server/Components/CellInput.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Sudoku.Web.Server.Components;
+
+public partial class CellInput
+{
+    [Parameter]
+    public int Column { get; set; }
+
+    [Parameter]
+    public int Row { get; set; }
+
+    [Parameter] public int? Value { get; set; }
+}
