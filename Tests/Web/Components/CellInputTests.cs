fce72aca9dae040186147d2b6b7256cf33f5c1f5
Shannon Stewart <shannon.stewart@outlook.com>
2023-09-16 12:00:40 -0600

    Added unit tests for CellInput component

---
+++ Tests/Web/Components/CellInputTests.cs
@@ -0,0 +1,42 @@
+﻿using Sudoku.Web.Server.Components;
+using Sudoku.Web.Server.EventHandlers;
+using XenobiaSoft.Sudoku;
+
+namespace UnitTests.Web.Components;
+
+public class CellInputTests : TestContext
+{
+	[Fact]
+	public void RenderingCellInput_WhenCellIsLocked_RendersLabel()
+	{
+		// Arrange
+        var cell = new Cell(0, 0)
+        {
+			Locked = true,
+			Value = 4
+        };
+
+		// Act
+        var renderComponent = RenderComponent<CellInput>(x => x.Add(p => p.Cell, cell));
+
+		// Assert
+        renderComponent.MarkupMatches("<td class=\"cell\"><label>4</label></td>");
+    }
+
+    [Fact]
+    public void RenderingCellInput_WhenCellIsNotLocked_RendersInput()
+    {
+        // Arrange
+        var cell = new Cell(0, 0)
+        {
+            Locked = false,
+            Value = 7
+        };
+
+        // Act
+        var renderedComponent = RenderComponent<CellInput>(x => x.Add(p => p.Cell, cell));
+
+        // Assert
+        renderedComponent.MarkupMatches("<td class=\"cell\"><input type=\"text\" maxlength=\"1\" value=\"7\" /></td>");
+    }
+}
--- Tests/Web/Shared/NavMenuTests.cs
+++ Tests/Web/Shared/NavMenuTests.cs
@@ -6,7 +6,7 @@ public class NavMenuTests : TestContext
 	public void NavMenu_RendersCorrectly()
 	{
 		// Arrange
-		var expected = "<div class=\"top-row ps-3 navbar navbar-dark\" ><div class=\"container-fluid\" ><a class=\"navbar-brand\" href=\"\" ><img alt=\"XenobiaSoft Sudoku\" src=\"images/logo.png\" ></a><button title=\"Navigation menu\" class=\"navbar-toggler\" ><span class=\"navbar-toggler-icon\" ></span></button></div>\r\n</div>\r\n<div class=\"collapse\" ><nav class=\"flex-column\" ><div class=\"nav-item px-3\" ><a href=\"\" class=\"nav-link active\"><i class=\"fa-solid fa-house\" ></i>Home</a></div></nav></div>";
+		var expected = "<div class=\"top-row ps-3 navbar navbar-dark\"><div class=\"container-fluid\"><a class=\"navbar-brand\" href=\"\"><img alt=\"XenobiaSoft Sudoku\" src=\"images/logo.png\"></a><button title=\"Navigation menu\" class=\"navbar-toggler\" ><span class=\"navbar-toggler-icon\"></span></button></div></div><div class=\"collapse nav-scrollable\" ><nav class=\"flex-column\"><div class=\"nav-item px-3\"><a href=\"/game\" class=\"nav-link\"><i class=\"fa-solid fa-play\"></i>&nbsp;New Game</a></div></nav></div>";
 
 		// Act
 		var content = RenderComponent<NavMenu>();
