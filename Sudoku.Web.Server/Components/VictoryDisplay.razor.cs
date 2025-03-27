b3cf08e8785a2b905e1226d33efd964254b94c8d
Shannon Stewart <shannon.stewart@outlook.com>
2025-03-27 14:58:09 -0600

    Added new win state

---
+++ Sudoku.Web.Server/Components/VictoryDisplay.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Sudoku.Web.Server.Components;
+
+public partial class VictoryDisplay
+{
+    [Parameter] public bool IsVictory { get; set; }
+    [Parameter] public TimeSpan ElapsedTime { get; set; } = TimeSpan.Zero;
+    [Parameter] public EventCallback NewGame { get; set; }
+}
---
+++ Tests/Web/Components/VictoryDisplayTests.cs
@@ -0,0 +1,60 @@
+﻿using Sudoku.Web.Server.Components;
+
+namespace UnitTests.Web.Components;
+
+public class VictoryDisplayTests : TestContext
+{
+    [Fact]
+    public void VictoryMessageIsDisplayedWhenIsVictoryIsTrue()
+    {
+        // Arrange
+        var isVictory = true;
+        var elapsedTime = new TimeSpan(0, 30, 0);
+
+        // Act
+        var sut = RenderComponent<VictoryDisplay>(parameters => parameters
+            .Add(p => p.IsVictory, isVictory)
+            .Add(p => p.ElapsedTime, elapsedTime)
+        );
+
+        // Assert
+        sut.Find(".victory-title").MarkupMatches("<h1 class=\"victory-title\">You Won!</h1>");
+    }
+
+    [Fact]
+    public void ElapsedTimeIsDisplayedCorrectly()
+    {
+        // Arrange
+        var isVictory = true;
+        var elapsedTime = new TimeSpan(1, 2, 3);
+
+        // Act
+        var sut = RenderComponent<VictoryDisplay>(parameters => parameters
+            .Add(p => p.IsVictory, isVictory)
+            .Add(p => p.ElapsedTime, elapsedTime)
+        );
+
+        // Assert
+        sut.Find(".victory-subtitle").MarkupMatches("<p class=\"victory-subtitle\">Time: 1:02:03</p>");
+    }
+
+    [Fact]
+    public void NewGameButtonIsRenderedAndClickable()
+    {
+        // Arrange
+        var isVictory = true;
+        var newGameCalled = false;
+        void NewGame() => newGameCalled = true;
+        var sut = RenderComponent<VictoryDisplay>(parameters => parameters
+            .Add(p => p.IsVictory, isVictory)
+            .Add(p => p.NewGame, NewGame)
+        );
+        var button = sut.Find("button.victory-button");
+
+        // Act
+        button.Click();
+
+        // Assert
+        newGameCalled.Should().BeTrue();
+    }
+}
