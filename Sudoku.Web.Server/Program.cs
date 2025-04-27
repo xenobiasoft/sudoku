35b0473e7713159ce4e122b577869c491cb1900f
Shannon Stewart <shannon.stewart@outlook.com>
2025-04-27 14:56:59 -0600

    Feature/add logging (#18)

--- Sudoku.Web.Server/Program.cs
+++ Sudoku.Web.Server/Program.cs
@@ -1,11 +1,20 @@
-using System.Reflection;
 using Azure.Identity;
 using BlazorApplicationInsights;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+ILogger<Program>? logger = null;
+
 try
 {
+    builder
+        .Logging
+        .ClearProviders()
+        .AddConsole()
+        .AddDebug()
+        .AddAzureWebAppDiagnostics();
+
     builder
         .Configuration
         .SetBasePath(Directory.GetCurrentDirectory())
@@ -22,31 +31,46 @@ try
         .RegisterBlazorGameServices()
         .AddBlazorApplicationInsights(x =>
         {
-            x.InstrumentationKey = builder.Configuration["ApplicationInsights:InstrumentationKey"];
+            x.InstrumentationKey = builder.Configuration["AppInsightsKey"];
+        })
+        .AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+        {
+            ConnectionString = builder.Configuration["AppInsightsConnectionString"]
+        })
+        .AddLogging(logging =>
+        {
+            logging
+                .AddApplicationInsights()
+                .AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Information)
+                .AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Warning);
         });
 
     var app = builder.Build();
 
+    logger = app.Services.GetRequiredService<ILogger<Program>>();
+
     if (!app.Environment.IsDevelopment())
     {
+        logger.LogInformation("Configuring exception handler and HSTS for production environment.");
         app.UseExceptionHandler("/Error");
         app.UseHsts();
     }
 
+    logger.LogInformation("Configuring middleware pipeline.");
     app.UseHttpsRedirection();
-
     app.UseStaticFiles();
-
     app.UseRouting();
 
+    logger.LogInformation("Mapping endpoints.");
     app.MapBlazorHub();
     app.MapFallbackToPage("/_Host");
     app.MapHealthChecks("/health-check");
 
+    logger.LogInformation("Starting the application.");
     app.Run();
 }
 catch (Exception ex)
 {
-    Console.WriteLine(ex);
+    logger?.LogError(ex, "An unhandled exception occurred.");
     throw;
 }
