0ae64576b4ee470886efd2a033b09cbf11f1cade
Shannon Stewart <shannon.stewart@outlook.com>
2025-07-12 12:59:16 -0600

    Fixed AliasGenerator

--- Sudoku/Services/PlayerService.cs
+++ Sudoku/Services/PlayerService.cs
@@ -10,8 +10,8 @@ public class PlayerService : IPlayerService
 
     public Task<string> CreateNewAsync()
     {
-        var adjective = RandomInstance.Next(Adjectives.Length);
-        var animal = RandomInstance.Next(Animals.Length);
+        var adjective = Adjectives[RandomInstance.Next(Adjectives.Length)];
+        var animal = Animals[RandomInstance.Next(Animals.Length)];
         var number = RandomInstance.Next(10, 100); // Add randomness to avoid collisions
 
         return Task.FromResult($"{adjective}{animal}{number}");
