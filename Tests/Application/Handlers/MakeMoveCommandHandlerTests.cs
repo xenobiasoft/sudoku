2a694c444b3a575fb4b690ffec6be702aaf5a9e0
Shannon Stewart <shannon.stewart@outlook.com>
2025-07-14 19:07:13 -0600

    Fixed broken build

--- Tests/Application/Handlers/MakeMoveCommandHandlerTests.cs
+++ Tests/Application/Handlers/MakeMoveCommandHandlerTests.cs
@@ -257,4 +257,19 @@ public class MakeMoveCommandHandlerTests : BaseTestByAbstraction<MakeMoveCommand
         game.StartGame();
         return game;
     }
+
+    private static List<Cell> InitializeCells(Func<int, int, Cell> cellFactory)
+    {
+        var cells = new List<Cell>();
+        
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                cells.Add(cellFactory(i, j));
+            }
+        }
+        
+        return cells;
+    }
 }
