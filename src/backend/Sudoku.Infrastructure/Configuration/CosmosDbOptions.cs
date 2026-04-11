ef80e4eeba4d9c42d5638ecc822591b568210c18
Shannon Stewart <shannon.stewart@outlook.com>
2026-04-11 13:46:31 -0600

    Switched production to use managed identity for the API to access Cosmos DB

--- src/backend/Sudoku.Infrastructure/Configuration/CosmosDbOptions.cs
+++ src/backend/Sudoku.Infrastructure/Configuration/CosmosDbOptions.cs
@@ -7,4 +7,6 @@ public class CosmosDbOptions
     public string ContainerName { get; set; } = "games";
     public string DatabaseName { get; set; } = "sudoku";
     public bool DisableSslValidation { get; set; }
+    public bool UseManagedIdentity { get; set; }
+    public string? AccountEndpoint { get; set; }
 }
