1901f528a8cb8b2852cd7933bd52df6bc8f97b66
Shannon Stewart <shannon.stewart@outlook.com>
2026-04-12 12:08:45 -0600

    Fixed name of Cosmos to the correct name

--- scripts/set-app-config.sh
+++ scripts/set-app-config.sh
@@ -47,7 +47,7 @@ set_key "UseCosmosDb"                   "true"
 set_key "CosmosDb:DatabaseName"         "sudoku"
 set_key "CosmosDb:ContainerName"        "games"
 set_key "CosmosDb:UseManagedIdentity"   "true"
-set_key "CosmosDb:AccountEndpoint"      "https://cosmos-sudoku-prod-westus2.documents.azure.com:443/"
+set_key "CosmosDb:AccountEndpoint"      "https://cosmos-sudoku-prod.documents.azure.com:443/"
 
 # -----------------------------------------------------------------------------
 # Azure Storage
